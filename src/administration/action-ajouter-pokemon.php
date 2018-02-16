3991a1b5ec5111da15b6a4225edc7c0892e041f4
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-02-16 09:18:09 -0500

    Filtration du formulaire ajouter

--- src/administration/action-ajouter-pokemon.php
+++ src/administration/action-ajouter-pokemon.php
@@ -2,8 +2,14 @@
 if(!empty($_POST['action-ajouter-pokemon']))
 {
 	//echo "action-ajouter-pokemon";
+	$filtrePokemon = array();
+	$filtrePokemon['nom'] = FILTER_SANITIZE_STRING;
+	$filtrePokemon['type'] = FILTER_SANITIZE_STRING;
+	$filtrePokemon['generation'] = FILTER_SANITIZE_NUMBER_INT;
+	$filtrePokemon['resume'] = FILTER_SANITIZE_STRING;
+	$pokemon = filter_var_array($_POST, $filtrePokemon); // $pokemon = filter_input_array(INPUT_POST, $filtrePokemon);
+	//print_r($pokemon);
 	include_once "../accesseur/PokemonDAO.php";
-	$pokemon = $_POST; // TODO : filtrer
 	$pokemonDao = new PokemonDAO();
 	$pokemonDao->ajouterPokemon($pokemon);	
 }
