b90dcf7259ce4f205ef2307b02b67b63be9a5795
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-02-20 14:55:36 -0500

    Données one-to-many : les apparitions des pokémons

--- data/mondepokemon.sql
+++ data/mondepokemon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le :  ven. 09 fév. 2018 à 06:05
+-- Généré le :  mar. 20 fév. 2018 à 11:53
 -- Version du serveur :  5.7.21
 -- Version de PHP :  7.1.13
 
@@ -21,6 +21,31 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `mondepokemon`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `apparition`
+--
+
+CREATE TABLE `apparition` (
+  `idApparition` int(11) NOT NULL,
+  `idPokemon` int(11) NOT NULL,
+  `episode` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `majeur` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `apparition`
+--
+
+INSERT INTO `apparition` (`idApparition`, `idPokemon`, `episode`, `description`, `majeur`) VALUES
+(1, 1, 247, 'Sa première apparition dans l\'animé se fait quand le Têtarte d\'Ondine évolue en Tarpaud dans l\'épisode 247, quand celui-ci rentre en contact avec la Roche Royale de Sacha. On ne verra ce dernier que très rarement dans la série.', 1),
+(2, 1, 249, 'Tammy en possède un appelé Bill dans l\'épisode 249.', 0),
+(3, 2, 746, 'Un Noacier qui appartient à un Agent Jenny apparaît dans l\'épisode 746.', 1),
+(4, 3, 140, 'La première apparition de Cizayox dans l\'animé se fait dans l\'épisode 140 \"Engagé pour Combattre\", où il affronte le Scarhino de Sacha', 1),
+(5, 3, 270, 'Régis en possède un que l\'on découvre dans l\'épisode 270 lors de la Conférence Argentée.', 1);
 
 -- --------------------------------------------------------
 
@@ -44,11 +69,17 @@ CREATE TABLE `pokemon` (
 INSERT INTO `pokemon` (`idPokemon`, `nom`, `type`, `generation`, `resume`, `illustration`) VALUES
 (1, 'Tarpaud', 'Eau', 2, 'Ce Pokémon est semblable à une grenouille arboricole, potentiellement inspirée de la rainette verte.', 'tarpaud.png'),
 (2, 'Noacier', 'Plante', 5, 'Noacier est un Pokémon ressemblant à une grosse bogue de marron fermée en acier et de forme ovale. Trois lianes partent du sommet de sa tête.', 'noacier.png'),
-(3, 'Cizayox', 'Acier', 2, 'Ses deux pinces font penser aux motifs que les papillons possèdent sur leurs ailes pour effrayer les prédateurs.', 'cizayox.png');
+(3, 'Cizayox5', 'Acier', 2, 'Ses deux pinces font penser aux motifs que les papillons possèdent sur leurs ailes pour effrayer les prédateurs.', 'cizayox.png');
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `apparition`
+--
+ALTER TABLE `apparition`
+  ADD PRIMARY KEY (`idApparition`);
 
 --
 -- Index pour la table `pokemon`
@@ -61,10 +92,16 @@ ALTER TABLE `pokemon`
 --
 
 --
+-- AUTO_INCREMENT pour la table `apparition`
+--
+ALTER TABLE `apparition`
+  MODIFY `idApparition` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT pour la table `pokemon`
 --
 ALTER TABLE `pokemon`
-  MODIFY `idPokemon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idPokemon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
