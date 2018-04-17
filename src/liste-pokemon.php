10712234dc51a122965f4e9671fe95108cdd6cd4
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-04-17 14:16:16 -0400

    4 étapes de l'ajax

--- src/liste-pokemon.php
+++ src/liste-pokemon.php
@@ -3,6 +3,7 @@
 	include "accesseur/PokemonDAO.php";
 	$pokemonDao = new PokemonDAO();
 	
+	$recherche = "";
 	if(!empty($_POST['action-rechercher']))
 	{ 	//print_r($_POST);
 		$recherche = filter_var($_POST['recherche'],FILTER_SANITIZE_STRING); 
@@ -23,13 +24,27 @@
 	<title>Monde des Pokémons</title>
 	<script type="text/javascript" src="lib/Ajax.js"></script>
 	<script type="text/javascript">
+		// ETAPE 1 - EVENEMENT
 		function rechercherLesSuggestions()
 		{
-			console.log('onkeyup');
+			console.log('#recherche.onkeyup');
+			
+			// ETAPE 2 - REQUETE
 			ajax = new Ajax();
 			//console.log(ajax);
 			url = 'http://localhost/monde-pokemons/src/action/suggestion.php';
+			ajax.executer("GET", url, "", recevoirLesSuggestions);
 			
+			
+		}
+		
+		// ETAPE 3 - RECEPTION (traitement)
+		function recevoirLesSuggestions()
+		{
+			
+			
+			
+			// ETAPE 4 - AFFICHAGE (retroaction)
 			
 		}
 	</script>
