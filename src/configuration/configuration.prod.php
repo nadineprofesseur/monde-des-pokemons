1e2a482441e5ebfa2167b5c783bd5062c34cd8ea
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-04-24 14:52:57 -0400

    Fichiers de configurations pour chaque environnement + courant non-versionné

---
+++ src/configuration/configuration.prod.php
@@ -0,0 +1,22 @@
+<?php
+
+	// Configurations de téléversement et médias
+	$site = new stdClass();
+	$site->chemin = new stdClass();
+	//$site->chemin->racine = $_SERVER['DOCUMENT_ROOT'] . '/monde-pokemons/src/';
+	$site->chemin->racine = 'C:\Bitnami\wampstack-7.1.13-1\apache2\htdocs\monde-pokemons\src\\';
+	$site->chemin->illustration = $site->chemin->racine . 'illustration\\';
+	$site->chemin->miniature = $site->chemin->racine . 'illustration\mini\\';
+	$site->url = new stdClass();
+	$site->url->racine = 'http://localhost/monde-pokemons/src/';
+	$site->url->illustration = 'http://localhost/monde-pokemons/src/illustration/';
+	$site->url->miniature = 'http://localhost/monde-pokemons/src/illustration/mini/';
+	
+	// Configurations pour la traduction
+	$traduction = new stdClass();
+	$traduction->chemin = $site->chemin->racine . 'locale/';
+	$traduction->domaine = 'messages';
+	$traduction->locale = 'en_CA.utf8'; // $locale = "en_CA";
+	$traduction->locales = ['fr_CA.utf8','en_CA.utf8'];
+	
+	
