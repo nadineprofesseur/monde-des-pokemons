f5985b6a0dceba8b10b934c85a4f0bf96a89b353
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-04-24 14:40:33 -0400

    Variables pour afficher les images et miniatures

--- src/configuration/configuration.php
+++ src/configuration/configuration.php
@@ -4,3 +4,10 @@
 	$site->chemin = new stdClass();
 	$site->chemin->racine = $_SERVER['DOCUMENT_ROOT'] . '/monde-pokemons/src/';
 	$site->chemin->illustration = $site->chemin->racine . 'illustration/';
+	$site->chemin->miniature = $site->chemin->racine . 'illustration/mini/';
+	$site->url = new stdClass();
+	$site->url->racine = 'http://localhost/monde-pokemons/src/';
+	$site->url->illustration = 'http://localhost/monde-pokemons/src/illustration/';
+	$site->url->miniature = 'http://localhost/monde-pokemons/src/illustration/mini/';
+	
+	
