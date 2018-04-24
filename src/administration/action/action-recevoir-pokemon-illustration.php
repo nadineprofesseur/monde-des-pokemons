0c271a9cb772b542136ea79b03358c96f4d8c767
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-04-24 14:38:20 -0400

    Factorisation dans un fichier de configuration

--- src/administration/action/action-recevoir-pokemon-illustration.php
+++ src/administration/action/action-recevoir-pokemon-illustration.php
@@ -1,9 +1,5 @@
 <?php
-	$site = new stdClass();
-	$site->chemin = new stdClass();
-	$site->chemin->racine = $_SERVER['DOCUMENT_ROOT'] . '/monde-pokemons/src/';
-	$site->chemin->illustration = $site->chemin->racine . 'illustration/';
-	
+	require_once "../configuration/configuration.php";
 	
 	//echo "POST : ";
 	//print_r($_POST);
---
+++ src/configuration/configuration.php
@@ -0,0 +1,6 @@
+<?php
+
+	$site = new stdClass();
+	$site->chemin = new stdClass();
+	$site->chemin->racine = $_SERVER['DOCUMENT_ROOT'] . '/monde-pokemons/src/';
+	$site->chemin->illustration = $site->chemin->racine . 'illustration/';
